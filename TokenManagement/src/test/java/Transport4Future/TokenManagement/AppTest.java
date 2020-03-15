9c02859a9bfd8c5e87d372c8f2eebd9601d02bf6
Raúl P <raporpe@gmail.com>
2020-03-15 11:30:41 +0100

    Updated some things

--- TokenManagement/src/test/java/Transport4Future/TokenManagement/AppTest.java
+++ TokenManagement/src/test/java/Transport4Future/TokenManagement/AppTest.java
@@ -25,4 +25,46 @@ public class AppTest
 		Assertions.assertThrows(TokenManagementException.class, ()-> tm.readTokenRequestFromJSON(filePath));
 		assertNotNull(filePath);
 	}
+	
+	@Test
+	public void TM_RF1_01_P1()
+	{
+		String filePath = "src/resources/CP-RF1-01.json";
+		TokenManager tm = new TokenManager();
+		try {
+			TokenRequest tr = tm.readTokenRequestFromJSON(filePath);
+			Assertions.assertTrue(tr.getDeviceName().length()<20);
+			Assertions.assertTrue(tr.getDeviceName().length()>=1);
+			Assertions.assertTrue(tr.getTypeDevice().equals("Sensor") || tr.getTypeDevice().equals("Actuator"));
+			Assertions.assertTrue(tr.getDriverVersion().matches("([0-9])(\.[0-9])*"));
+			Assertions.assertTrue(tr.getSerialNumber().matches("[0-z]+(\/[0-z]+)+"));
+			Assertions.assertTrue(tr.getDriverVersion().length()<50);
+			Assertions.assertTrue(tr.getMacAddress().matches("[0-Z][0-Z])(:[0-Z][0-Z]+");
+			Assertions.assertTrue(tr.getMacAddress().length()==17;
+		}catch(TokenManagementException a) {
+			System.out.println(a.getMessage());
+		}
+	}
+	
+	@Test
+	public void CP_RF1_02_I1()
+	{
+		String filePath = "src/resources/CP-RF1-01.json";
+		TokenManager tm = new TokenManager();
+		try {
+			TokenRequest tr = tm.readTokenRequestFromJSON(filePath);
+			Assertions.assertTrue(tr.getDeviceName().length()<20);
+			Assertions.assertTrue(tr.getDeviceName().length()>=1);
+			Assertions.assertTrue(tr.getTypeDevice().equals("Sensor") || tr.getTypeDevice().equals("Actuator"));
+			Assertions.assertTrue(tr.getDriverVersion().matches("([0-9])(\.[0-9])*"));
+			Assertions.assertTrue(tr.getSerialNumber().matches("[0-z]+(\/[0-z]+)+"));
+			Assertions.assertTrue(tr.getDriverVersion().length()<50);
+			Assertions.assertTrue(tr.getMacAddress().matches("[0-Z][0-Z])(:[0-Z][0-Z]+");
+			Assertions.assertTrue(tr.getMacAddress().length()==17;
+		}catch(TokenManagementException a) {
+			System.out.println(a.getMessage());
+		}
+	}
+	
+	
 }
