2c6f81a8f3224a807c560111a320595a7ceb189d
Raúl P <raporpe@gmail.com>
2020-03-16 19:25:45 +0100

    Some changes

--- TokenManagement/src/test/java/Transport4Future/TokenManagement/AppTest.java
+++ TokenManagement/src/test/java/Transport4Future/TokenManagement/AppTest.java
@@ -2,69 +2,150 @@ package Transport4Future.TokenManagement;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.math.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import javax.json.Json;
+import javax.json.JsonObject;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest
-{
-	
-	/* Test case: CP_RF1_01
-	* Equivalence class or boundary value considered: <CE-RF1-V-01
-	* Testing technique: Equivalence Class
-	* Expected value: Pass 
-	*/
+public class AppTest {
 
-	@Test
-	public void TM_RF1_01()
-	{
-		String filePath = "resources/CP-RF1-01.json";
-		TokenManager tm = new TokenManager();
-		Assertions.assertThrows(TokenManagementException.class, ()-> tm.readTokenRequestFromJSON(filePath));
+	/*
+	 * Test case: CP_RF1_01 Equivalence class or boundary value considered:
+	 * <CE-RF1-V-01 Testing technique: Equivalence Class Expected value: Pass
+	 */
+
+	private JsonObject json;
+	private TokenManager tm;
+	private String filePath;
+	private TokenRequest rq;
+	private String password;
+
+	@BeforeAll
+	public void TM_RF1_01() {
+		filePath = "src/resources/CP-RF1-01.json";
+		json = readJSON(filePath);
+		tm = new TokenManager();
+
+		// idk
+		Assertions.assertThrows(TokenManagementException.class, () -> tm.readTokenRequestFromJSON(filePath));
 		assertNotNull(filePath);
 	}
-	
+
 	@Test
-	public void TM_RF1_01_P1()
-	{
-		String filePath = "src/resources/CP-RF1-01.json";
-		TokenManager tm = new TokenManager();
-		try {
-			TokenRequest tr = tm.readTokenRequestFromJSON(filePath);
-			Assertions.assertTrue(tr.getDeviceName().length()<20);
-			Assertions.assertTrue(tr.getDeviceName().length()>=1);
-			Assertions.assertTrue(tr.getTypeDevice().equals("Sensor") || tr.getTypeDevice().equals("Actuator"));
-			Assertions.assertTrue(tr.getDriverVersion().matches("([0-9])(.[0-9])*"));
-			Assertions.assertTrue(tr.getSerialNumber().matches("[0-z]+(/[0-z]+)+"));
-			Assertions.assertTrue(tr.getDriverVersion().length()<50);
-			Assertions.assertTrue(tr.getMacAddress().matches("[0-Z][0-Z])(:[0-Z][0-Z]+"));
-			Assertions.assertTrue(tr.getMacAddress().length()==17);
-		}catch(TokenManagementException a) {
-			System.out.println(a.getMessage());
-		}
+	public void TM_RF1_02_I1() {
+
+		Assertions.assertTrue(json.getString("Device Name").length() < 20);
+		Assertions.assertTrue(json.getString("Device Name").length() >= 1);
+		Assertions.assertTrue(json.getString("Type of Device").equals("Sensor")
+				|| json.getString("Type of Device").equals("Actuator"));
+		Assertions.assertTrue(json.getString("Driver Version").matches("([0-9])(.[0-9])*"));
+		Assertions.assertTrue(json.getString("Driver Version").matches("[0-z]+(/[0-z]+)+"));
+		Assertions.assertTrue(json.getString("Serial Number").length() < 50);
+		Assertions.assertTrue(json.getString("MAC Address").matches("^([a-fA-F0-9]{2}[:-]){5}[a-fA-F0-9]{2}$"));
+		Assertions.assertTrue(json.getString("MAC Address").length() == 17);
+
 	}
-	
+
 	@Test
-	public void TM_RF1_02_I1()
-	{
-		String filePath = "src/resources/CP-RF1-01.json";
-		TokenManager tm = new TokenManager();
+	public void TM_RF1_01_P1() {
 		try {
-			TokenRequest tr = tm.readTokenRequestFromJSON(filePath);
-			Assertions.assertTrue(tr.getDeviceName().length()<20);
-			Assertions.assertTrue(tr.getDeviceName().length()>=1);
-			Assertions.assertTrue(tr.getTypeDevice().equals("Sensor") || tr.getTypeDevice().equals("Actuator"));
-			Assertions.assertTrue(tr.getDriverVersion().matches("([0-9])(.[0-9])*"));
-			Assertions.assertTrue(tr.getSerialNumber().matches("[0-z]+(/[0-z]+)+"));
-			Assertions.assertTrue(tr.getDriverVersion().length()<50);
-			Assertions.assertTrue(tr.getMacAddress().matches("[0-Z][0-Z])(:[0-Z][0-Z]+"));
-			Assertions.assertTrue(tr.getMacAddress().length()==17);
-		}catch(TokenManagementException a) {
-			System.out.println(a.getMessage());
+			rq = tm.readTokenRequestFromJSON(filePath);
+		} catch (TokenManagementException a) {
+			Assertions.fail("The json file was not found");
 		}
+
+		Assertions.assertEquals(rq.getDeviceName(), json.getString("Device Name"));
+		Assertions.assertEquals(rq.getTypeDevice(), json.getString("Type of Device"));
+		Assertions.assertEquals(rq.getDriverVersion(), json.getString("Driver Version"));
+		Assertions.assertEquals(rq.getSerialNumber(), json.getString("Serial Number"));
+		Assertions.assertEquals(rq.getMacAddress(), json.getString("MAC Address"));
+
 	}
-	
-	
+
+	@Test
+	public void TM_RF_01_P2() throws TokenManagementException {
+		
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			throw new TokenManagementException("Error: no such hashing algorithm.");
+		}
+		
+		//  Defined  password is "Stardust" & req is the TokenRequest object
+		String input =  password + "-" + rq.toString();
+		
+		md.update(input.getBytes(StandardCharsets.UTF_8));
+		byte[] digest = md.digest();
+
+		// Beware the hex length. If MD5 -> 32:"%032x", but for instance, in SHA-256 it should be "%064x" 
+		String hex = String.format("%32x", new BigInteger(1, digest));
+
+		Assertions.assertEquals(hex, rq.toString());
+		
+
+	}
+
+	private JsonObject readJSON(String path) {
+
+		String fileContents = "";
+		BufferedReader reader = null;
+
+		try {
+			reader = new BufferedReader(new FileReader(path));
+		} catch (FileNotFoundException e) {
+			Assertions.fail("The json file was not found");
+		}
+
+		String line;
+		try {
+			while ((line = reader.readLine()) != null) {
+				fileContents += line;
+			}
+		} catch (IOException e) {
+			Assertions.fail("Error: input file could not be accessed.");
+		}
+
+		try {
+			reader.close();
+		} catch (IOException e) {
+			Assertions.fail("Error: input file could not be closed.");
+		}
+
+		JsonObject jsonLicense = Json.createReader(new StringReader(fileContents)).readObject();
+		DateFormat df = new SimpleDateFormat("EEE MMM dd kk:mm:ss z yyyy", Locale.ENGLISH);
+		try {
+
+			jsonLicense.getString("Device Name");
+			jsonLicense.getString("Type of Device");
+			jsonLicense.getString("Driver Version");
+			jsonLicense.getString("Serial Number");
+			jsonLicense.getString("MAC Address");
+
+		} catch (Exception e) {
+			Assertions.fail("Error: invalid input data in JSON structure.");
+		}
+
+		return jsonLicense;
+	}
+
 }
