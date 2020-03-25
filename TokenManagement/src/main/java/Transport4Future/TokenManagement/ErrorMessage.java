8fe8dc4e2bb8ba0f69836643de06285de46052e7
Raúl P <raporpe@gmail.com>
2020-03-25 19:56:34 +0100

    Updated error messages

--- TokenManagement/src/main/java/Transport4Future/TokenManagement/ErrorMessage.java
+++ TokenManagement/src/main/java/Transport4Future/TokenManagement/ErrorMessage.java
@@ -2,37 +2,41 @@ package Transport4Future.TokenManagement;
 
 public class ErrorMessage {
 
-    // Field invalid format
-    public static String driverVersionInvalidFormat = "The driverVersion field uses an invalid format.";
-    public static String typeDeviceInvalidFormat = "Error: typeDevice uses an invalid format.";
-    public static String serialNumberInvalidFormat = "The serialNumber field uses an invalid format.";
-    public static String macAddressInvalidFormat = "The macAddress field uses an invalid format.";
-    public static String emailInvalidFormat = "The email is in an invalid format.";
-    public static String deviceNameInvalidFormat = "Error: deviceName uses an invalid format.";
+    // Tag invalid format part 1
+    public static String driverVersionInvalidFormat = "The driverVersion tag uses an invalid format.";
+    public static String typeDeviceInvalidFormat = "The typeDevice tag uses an invalid format.";
+    public static String serialNumberInvalidFormat = "The serialNumber tag uses an invalid format.";
+    public static String macAddressInvalidFormat = "The macAddress tag uses an invalid format.";
+    public static String emailInvalidFormat = "The email tag is in an invalid format.";
+    public static String deviceNameInvalidFormat = "The deviceName tag uses an invalid format.";
 
-    public static String jsonParsingError = "Error: error reading the json file.";
-    public static String jsonExtraTagError = "An extra tag was detected";
-    public static String closeFileError = "Error: input file could not be closed.";
-    public static String readFileError = "Error: input file could not be read.";
-    public static String inputFileNotFoundError = "Error: input file not found.";
-    public static String jsonMissingTagError = "Could not find the json field ";
-    public static String invalidDateFormat = "The date is not in the correct format";
+    // JSON-related read errors
+    public static String jsonParsingError = "Error parsing the json file.";
+    public static String jsonExtraTagError = "An extra json tag was detected";
+    public static String jsonMissingTagError = "Could not find the json tag ";
+    public static String emptyFileError = "The file is empty";
 
-    //Algorithm errors
-    public static String md5AlgorithmNotFound = "The MD5 algorithm was not found";
-    public static String sha256AlgorithmNotFound = "The SHA-256 algorithm was not found";
+    // File operation errors
+    public static String closeFileError = "Input file could not be closed.";
+    public static String readFileError = "Input file could not be read.";
+    public static String inputFileNotFoundError = "Input file not found.";
 
-    //Token store
+    // Hash algorithm errors
+    public static String md5AlgorithmNotFound = "The MD5 hashing algorithm was not found";
+    public static String sha256AlgorithmNotFound = "The SHA-256 hashing algorithm was not found";
 
+    // Token store errors
     public static String storeDirCreationError = "Could not create the directory 'store'";
-    public static String jsonStoreFileCreationError = "Could not create the license store file tokenStore.json";
-    public static String closeStoreFileError = "Error: Unable to open internal licenses store file";
-    public static String writeStoreFileError = "Error: Unable to write to license store file";
-    public static String readStoreFileError = "Error: Unable to read to license store file";
+    public static String jsonStoreFileCreationError = "Could not create the license store file 'tokenStore.json'";
+    public static String closeStoreFileError = "Unable to open the licenses store file";
+    public static String writeStoreFileError = "Unable to write to the license store file";
+    public static String readStoreFileError = "Unable to read the license store file";
 
-    public static String base64EncodingError = "Error enconding 64URL.";
-    public static String emptyFileError = "Empasdkhfkas";
+    // Base 64 encoding
+    public static String base64EncodingError = "Error encoding the string in Base64.";
 
+    // Part 2
+    public static String invalidDateFormat = "The date is not in the correct format";
 
 
 
