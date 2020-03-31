c5ece7fec5e42e6c6fd0e84fa9f4226c35cc5af1
dgpMakes <43809495+dgpMakes@users.noreply.github.com>
2020-04-01 00:56:04 +0200

    apptest3

---
+++ TokenManagement/src/test/java/Transport4Future/TokenManagement/AppTest_3.java
@@ -0,0 +1,20 @@
+package Transport4Future.TokenManagement;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest_3 {
+
+	public AppTest_3() {
+		
+	}
+
+	
+	 void testfr(){
+
+		 
+	    
+	 }
+}
